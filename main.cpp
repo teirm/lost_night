8bb1e9fc3ded7ad76c74661493579307b4182f32
teirm <tribunus.vii@gmail.com>
2016-09-02 18:27:19 -0400

    Moved main program to C++

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Main program for lost_night text based
+// RPG
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	cout << "Lost Night" << endl;
+
+	return 0;
+}
